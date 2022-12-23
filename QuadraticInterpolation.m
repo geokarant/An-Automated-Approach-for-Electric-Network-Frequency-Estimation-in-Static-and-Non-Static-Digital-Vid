d0d0988e904f99c20715100199ac8293af90c0e7
geokarant <karantaidisg@gmail.com>
2022-12-23 13:28:34 +0200

    Update QuadraticInterpolation.m

--- QuadraticInterpolation.m
+++ QuadraticInterpolation.m
@@ -1,9 +1,5 @@
 function [ delta ] = QuadraticInterpolation( power_vector, index,fS )
 
-
-
-
-
 for uu=1:numel(index);
 
 alpha(1,uu) = ((power_vector((index(uu) - 1),uu)));
@@ -12,6 +8,5 @@ gamma(1,uu) =((power_vector((index(uu) + 1),uu)));
 delta1(1,uu) = (0.5*(alpha(1,uu) - gamma(1,uu)))./(alpha(1,uu) - 2*beta(1,uu) + gamma(1,uu));
 
 delta(1,uu)=delta1(1,uu).*(fS(index(1,uu)+1)-fS(index(1,uu)))';
-
 
 end
